52da733098fce52e25add897f72baa0e1ca24d22
Geoffroy Couprie <geo.couprie@gmail.com>
2018-04-16 14:34:20 +0200

    remove unused functions

--- src/host.rs
+++ src/host.rs
@@ -56,50 +56,18 @@ impl TestHost {
     }
 }
 
-/// sub(a: i32, b: i32) -> i32
-///
-/// This function just substracts one integer from another,
-/// returning the subtraction result.
-const SUB_FUNC_INDEX: usize = 0;
-
-/// err(error_code: i32) -> !
-///
-/// This function traps upon a call.
-/// The trap have a special type - HostErrorWithCode.
-const ERR_FUNC_INDEX: usize = 1;
-
-/// inc_mem(ptr: *mut u8)
-///
-/// Increments value at the given address in memory. This function
-/// requires attached memory.
-const INC_MEM_FUNC_INDEX: usize = 2;
-
-/// get_mem(ptr: *mut u8) -> u8
-///
-/// Returns value at the given address in memory. This function
-/// requires attached memory.
-const GET_MEM_FUNC_INDEX: usize = 3;
-
-/// recurse<T>(val: T) -> T
-///
-/// If called, resolves exported function named 'recursive' from the attached
-/// module instance and then calls into it with the provided argument.
-/// Note that this function is polymorphic over type T.
-/// This function requires attached module instance.
-const RECURSE_FUNC_INDEX: usize = 4;
-
 /// log(ptr: *mut u8, size: u64)
 ///
 /// Returns value at the given address in memory. This function
 /// requires attached memory.
-const LOG_INDEX: usize = 5;
+const LOG_INDEX: usize = 0;
 
-const RESPONSE_SET_STATUS_LINE: usize = 6;
-const RESPONSE_SET_HEADER: usize = 7;
-const RESPONSE_SET_BODY: usize = 8;
-const TCP_CONNECT: usize = 9;
-const TCP_READ: usize = 10;
-const TCP_WRITE: usize = 11;
+const RESPONSE_SET_STATUS_LINE: usize = 1;
+const RESPONSE_SET_HEADER: usize = 2;
+const RESPONSE_SET_BODY: usize = 3;
+const TCP_CONNECT: usize = 4;
+const TCP_READ: usize = 5;
+const TCP_WRITE: usize = 6;
 
 impl Externals for TestHost {
     fn invoke_index(
@@ -108,63 +76,6 @@ impl Externals for TestHost {
         args: RuntimeArgs,
     ) -> Result<Option<RuntimeValue>, Trap> {
         match index {
-            SUB_FUNC_INDEX => {
-                let a: i32 = args.nth(0);
-                let b: i32 = args.nth(1);
-
-                let result: RuntimeValue = (a - b).into();
-
-                Ok(Some(result))
-            }
-            ERR_FUNC_INDEX => {
-                let error_code: u32 = args.nth(0);
-                let error = HostErrorWithCode { error_code };
-                Err(TrapKind::Host(Box::new(error)).into())
-            }
-            INC_MEM_FUNC_INDEX => {
-                let ptr: u32 = args.nth(0);
-
-                let memory = self.memory
-                    .as_ref()
-                    .expect("Function 'inc_mem' expects attached memory");
-                let mut buf = [0u8; 1];
-                memory.get_into(ptr, &mut buf).unwrap();
-                buf[0] += 1;
-                memory.set(ptr, &buf).unwrap();
-
-                Ok(None)
-            }
-            GET_MEM_FUNC_INDEX => {
-                let ptr: u32 = args.nth(0);
-
-                let memory = self.memory
-                    .as_ref()
-                    .expect("Function 'get_mem' expects attached memory");
-                let mut buf = [0u8; 1];
-                memory.get_into(ptr, &mut buf).unwrap();
-
-                Ok(Some(RuntimeValue::I32(buf[0] as i32)))
-            }
-            RECURSE_FUNC_INDEX => {
-                let val = args.nth_value_checked(0)
-                    .expect("Exactly one argument expected");
-
-                let instance = self.instance
-                    .as_ref()
-                    .expect("Function 'recurse' expects attached module instance")
-                    .clone();
-                let result = instance
-                    .invoke_export("recursive", &[val.into()], self)
-                    .expect("Failed to call 'recursive'")
-                    .expect("expected to be Some");
-
-                if val.value_type() != result.value_type() {
-                    return Err(
-                        TrapKind::Host(Box::new(HostErrorWithCode { error_code: 123 })).into(),
-                    );
-                }
-                Ok(Some(result))
-            },
             LOG_INDEX => {
               let ptr: u32 = args.nth(0);
               let sz:  u64 = args.nth(1);
@@ -291,20 +202,7 @@ impl Externals for TestHost {
 
 impl TestHost {
     fn check_signature(&self, index: usize, signature: &Signature) -> bool {
-        if index == RECURSE_FUNC_INDEX {
-            // This function requires special handling because it is polymorphic.
-            if signature.params().len() != 1 {
-                return false;
-            }
-            let param_type = signature.params()[0];
-            return signature.return_type() == Some(param_type);
-        }
-
         let (params, ret_ty): (&[ValueType], Option<ValueType>) = match index {
-            SUB_FUNC_INDEX => (&[ValueType::I32, ValueType::I32], Some(ValueType::I32)),
-            ERR_FUNC_INDEX => (&[ValueType::I32], None),
-            INC_MEM_FUNC_INDEX => (&[ValueType::I32], None),
-            GET_MEM_FUNC_INDEX => (&[ValueType::I32], Some(ValueType::I32)),
             LOG_INDEX => (&[ValueType::I32, ValueType::I64], None),
             RESPONSE_SET_STATUS_LINE => (&[ValueType::I32, ValueType::I32, ValueType::I64], None),
             RESPONSE_SET_HEADER => (&[ValueType::I32, ValueType::I64, ValueType::I32, ValueType::I64], None),
@@ -322,11 +220,6 @@ impl TestHost {
 impl ModuleImportResolver for TestHost {
     fn resolve_func(&self, field_name: &str, signature: &Signature) -> Result<FuncRef, Error> {
         let index = match field_name {
-            "sub" => SUB_FUNC_INDEX,
-            "err" => ERR_FUNC_INDEX,
-            "inc_mem" => INC_MEM_FUNC_INDEX,
-            "get_mem" => GET_MEM_FUNC_INDEX,
-            "recurse" => RECURSE_FUNC_INDEX,
             "log" => LOG_INDEX,
             "response_set_status_line" => RESPONSE_SET_STATUS_LINE,
             "response_set_header" => RESPONSE_SET_HEADER,
