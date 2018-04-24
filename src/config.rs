40efe2422f6d4538ec65f61f4e8952689a3254db
Geoffroy Couprie <geo.couprie@gmail.com>
2018-04-24 18:44:14 +0200

    move ApplicationState

--- src/config.rs
+++ src/config.rs
@@ -2,6 +2,8 @@ use std::fs::File;
 use std::io::Read;
 use std::collections::HashMap;
 use toml;
+use vm::load_module;
+use wasmi::Module;
 
 #[derive(Deserialize,Debug)]
 pub struct WasmApp {
@@ -30,3 +32,44 @@ pub fn load(file: &str) -> Option<Config> {
   }
   None
 }
+
+pub struct ApplicationState {
+  /// (method, url path) -> (function name, module path, env)
+  pub routes: HashMap<(String, String), (String, String, Option<HashMap<String, String>>)>,
+  /// module path -> Module
+  pub modules: HashMap<String, Module>,
+}
+
+impl ApplicationState {
+  pub fn new(config: &Config) -> ApplicationState {
+    let mut routes  = HashMap::new();
+    let mut modules = HashMap::new();
+
+    for app in config.applications.iter() {
+      //FIXME: it might be good to not panic when we don't find the function in the module
+      let module = load_module(&app.file_path, &app.function);
+
+      if !modules.contains_key(&app.file_path) {
+        modules.insert(app.file_path.clone(), module);
+      }
+
+      routes.insert((app.method.clone(), app.url_path.clone()), (app.function.clone(), app.file_path.clone(), app.env.clone()));
+    }
+
+    ApplicationState {
+      routes:  routes,
+      modules: modules,
+    }
+  }
+
+  pub fn route(&self, method: &str, url: &str) -> Option<(&str, &Module, &Option<HashMap<String, String>>)> {
+    if let Some((func_name, module_path, ref opt_env)) = self.routes.get(&(method.to_string(), url.to_string())) {
+      if let Some(module) = self.modules.get(module_path) {
+        return Some((func_name, module, opt_env));
+      }
+    }
+
+    None
+  }
+}
+
--- src/vm.rs
+++ src/vm.rs
@@ -6,48 +6,8 @@ use rouille;
 use std::collections::HashMap;
 
 use super::host;
-use config::Config;
-use interpreter::{create_stack, my_run_interpreter_loop, WasmInstance};
-
-pub struct ApplicationState {
-  /// (method, url path) -> (function name, module path, env)
-  pub routes: HashMap<(String, String), (String, String, Option<HashMap<String, String>>)>,
-  /// module path -> Module
-  pub modules: HashMap<String, Module>,
-}
-
-impl ApplicationState {
-  pub fn new(config: &Config) -> ApplicationState {
-    let mut routes  = HashMap::new();
-    let mut modules = HashMap::new();
-
-    for app in config.applications.iter() {
-      //FIXME: it might be good to not panic when we don't find the function in the module
-      let module = load_module(&app.file_path, &app.function);
-
-      if !modules.contains_key(&app.file_path) {
-        modules.insert(app.file_path.clone(), module);
-      }
-
-      routes.insert((app.method.clone(), app.url_path.clone()), (app.function.clone(), app.file_path.clone(), app.env.clone()));
-    }
-
-    ApplicationState {
-      routes:  routes,
-      modules: modules,
-    }
-  }
-
-  pub fn route(&self, method: &str, url: &str) -> Option<(&str, &Module, &Option<HashMap<String, String>>)> {
-    if let Some((func_name, module_path, ref opt_env)) = self.routes.get(&(method.to_string(), url.to_string())) {
-      if let Some(module) = self.modules.get(module_path) {
-        return Some((func_name, module, opt_env));
-      }
-    }
-
-    None
-  }
-}
+use config::{Config, ApplicationState};
+use interpreter::WasmInstance;
 
 pub fn server(config: Config) {
     let state = ApplicationState::new(&config);
